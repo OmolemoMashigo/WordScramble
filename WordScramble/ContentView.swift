0dda58b010ac21eb0184434f52619d2e26ff4d97
Omolemo Mashigo <omomashigo@gmail.com>
2025-02-18 22:12:49 +0200

    functionality of adding a list of words with basic formatting

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -9,14 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let boys = ["Thabo", "Rey", "Matt"]
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
     
     var body: some View {
-        //if list is only made up of dynamic content
-        List(boys, id: \.self){
-            Text($0)
+        NavigationStack{
+            List{
+                Section{
+                    TextField("Enter your word:", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                }
+                
+                Section{
+                    ForEach(usedWords, id: \.self){ word in
+                        HStack{
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+        }.navigationTitle(rootWord)
+            .onSubmit() {
+                addNewWord()
+            }
+        
+    }
+
+    func addNewWord(){
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines) //prevents adding duplicate words with case differences
+        
+        //exit if remaining string is empty
+        guard answer.count > 0 else{return}
+        
+        //insert newWord at pos 0 instead of appending so that new words appear in the beginning of the list instead of the end
+       
+        withAnimation{
+            usedWords.insert(newWord, at: 0)
         }
         
+        //clear newWord back to being an empty string
+        newWord = ""
     }
 }
 
